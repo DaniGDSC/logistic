bfd7fa61207d9cf5710d85ebfb0511311ede35f1
Dani <gdschanu.hieund1601@gmail.com>
2024-09-09 12:46:31 +0700

    db setting

--- inventory_BE/inventory_BE/settings.py
+++ inventory_BE/inventory_BE/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = "inventory_BE.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'inventory',
+            'USER': 'root',
+            'PASSWORD': 'Gdsc.1201@',
+            'HOST': 'localhost',  # or the MySQL server IP address
+            'PORT': '3306',
+         }
+     }
 
 
 # Password validation
