4330a556ea95091914e6991fa45d51f448d6cc2e
Dani <gdschanu.hieund1601@gmail.com>
2024-09-09 18:43:00 +0700

    add

--- inventory-fe/src/Header.js
+++ inventory-fe/src/Header.js
@@ -9,9 +9,12 @@ const Header = () => {
           <h1 className="text-2xl font-bold">Inventory Management</h1>
           <nav>
             <ul className="flex space-x-4">
-                <li><Link to="./app.js" className="hover:underline">Dashboard</Link></li> {/* Use the imported Link component */}
+              <li><Link to="./app.js" className="hover:underline">Dashboard</Link></li> {/* Use the imported Link component */}
               <li><Link to="./page/ProductPage.js" className="hover:underline">Product</Link></li>
               <li><Link to="/orders" className="hover:underline">Orders</Link></li>
+              <li><Link to="/orders" className="hover:underline">Suppliers</Link></li>
+              <li><Link to="/orders" className="hover:underline">Reports</Link></li>
+              <li><Link to="/orders" className="hover:underline">User Management</Link></li>
             </ul>
           </nav>
         </div>
