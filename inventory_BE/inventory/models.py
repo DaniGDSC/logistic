8b991be3861d61acfb47e5f2eea95593ce49835e
Dani <gdschanu.hieund1601@gmail.com>
2024-09-09 12:51:50 +0700

    model

--- inventory_BE/inventory/models.py
+++ inventory_BE/inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
